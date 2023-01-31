d0f4d3e6c091583d1d71d46bf1f57282a0e2cab5
Mike Ross <mikeross36mikeross@gmail.com>
2023-01-31 09:32:27 +0100

    project items added

--- src/components/projects/data.js
+++ src/components/projects/data.js
@@ -2,10 +2,13 @@ import nextId from "react-id-generator"
 import coffeebar from "../../images/coffeebar.png"
 import drmartens from "../../images/drmartens.png"
 import drones from "../../images/drones.png"
+import dronesDark from "../../images/drones-dark.png"
 import fitness from "../../images/fitness.png"
 import eye from "../../images/eye-icon.svg"
 import github from "../../images/github-icon.svg"
 import ganja from "../../images/ganjawebshop.png"
+import sandwitch from "../../images/sandwitch.png"
+import michelangelo from "../../images/michelangelo.png"
 
 
 export const projects = [
@@ -38,6 +41,38 @@ export const projects = [
         image: fitness,
         btns: [
             { id: nextId(), url: "https://fitness-truth-website.netlify.app/", icon: eye },
+            {id: nextId(), url: "https://github.com/mikeross36", icon: github}
+        ]
+    },
+    {
+        id: nextId(),
+        image: coffeebar,
+        btns: [
+            { id: nextId(), url: "https://coffeebar-website.netlify.app", icon: eye },
+            {id: nextId(), url: "https://github.com/mikeross36", icon: github}
+        ]
+    },
+    {
+        id: nextId(),
+        image: dronesDark,
+        btns: [
+            { id: nextId(), url: "https://drones-website.netlify.app", icon: eye },
+            {id: nextId(), url: "https://github.com/mikeross36", icon: github}
+        ]
+    },
+    {
+        id: nextId(),
+        image: sandwitch,
+        btns: [
+            { id: nextId(), url: "https://sandwich-bar.netlify.app", icon: eye },
+            {id: nextId(), url: "https://github.com/mikeross36", icon: github}
+        ]
+    },
+    {
+        id: nextId(),
+        image: michelangelo,
+        btns: [
+            { id: nextId(), url: "https://caffe-michelangelo-ns.netlify.app", icon: eye },
             {id: nextId(), url: "https://github.com/mikeross36", icon: github}
         ]
     },
